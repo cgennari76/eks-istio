6077dac9236d6b0e58a847e6093febd9360a9a3d
root <root@PC00076.F86PLATYPUS.COM>
2025-01-26 14:30:15 -0800

    update helm script

--- helmsetup.sh
+++ helmsetup.sh
@@ -1,3 +1,4 @@
 curl -OL https://get.helm.sh/helm-v3.17.0-linux-arm64.tar.gz
 tar -zxvf helm-v3.17.0-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
+helm repo add istio https://istio-release.storage.googleapis.com/charts
